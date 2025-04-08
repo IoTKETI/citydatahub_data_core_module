0bbc1554072bed4178942152a059b59bdec4fc9e
Seongyun Kim <seongyun.kim@keti.re.kr>
2025-04-08 21:29:22 +0900

    데이터 순서 보장을 위한 수정

--- ingestinterface/src/main/java/kr/re/keti/sc/ingestinterface/controller/kafka/producer/DefaultKafkaProducer.java
+++ ingestinterface/src/main/java/kr/re/keti/sc/ingestinterface/controller/kafka/producer/DefaultKafkaProducer.java
@@ -2,6 +2,7 @@ package kr.re.keti.sc.ingestinterface.controller.kafka.producer;
 
 
 import java.util.Properties;
+import java.util.UUID;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -28,9 +29,11 @@ public class DefaultKafkaProducer implements IKafkaProducer {
 	@Override
 	public void send(String topic, byte[] message) {
 
-		log.info("KafkaProduce topic=" + topic + ", Message=" + new String(message));
+		int key = UUID.randomUUID().hashCode();
+		log.info("KafkaProduce topic=" + topic + ", Key=" + key +", Message=" + new String(message));
 
-		producer.send(new ProducerRecord<>(topic, message));
+		//데이터 처리 순서 보장을 위하여 Kafka Key 값 활용하여 하나의 Partition으로 전송
+		producer.send(new ProducerRecord<>(topic, key, message));
 	}
 
 	@Override
